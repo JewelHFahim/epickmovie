3eda052b757819174025271afa83d9f6f90c60f8
JewelHFahim <jewelhfahim@gmail.com>
2024-01-20 16:24:23 +0600

    tv setting up

--- src/dashboard/settings/TvshowSort.jsx
+++ src/dashboard/settings/TvshowSort.jsx
@@ -5,8 +5,8 @@ const TvshowSort = ({ selectedOptionTv, setSelectedOptionTv, tv_order }) => {
   const options = [
     { label: "ASC", value: "asc" },
     { label: "DESC", value: "desc" },
-    { label: "Release Date", value: "release_date" },
-    { label: "Latest Post", value: "latest_post" },
+    { label: "Release Date ASC", value: "release_date_asc" },
+    { label: "Release Date DESC", value: "release_date_desc" },
   ];
 
   return (
