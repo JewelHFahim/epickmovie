6a97e98183ca5ada3c8fb6d0c67e26e1602a5a5c
JewelHFahim <jewelhfahim@gmail.com>
2024-01-20 16:22:29 +0600

    updated

--- src/dashboard/settings/MovieSort.jsx
+++ src/dashboard/settings/MovieSort.jsx
@@ -4,8 +4,8 @@ const MovieSort = ({ selectedOptionMovie, setSelectedOptionMovie, movie_order })
   const options = [
     { label: "ASC", value: "asc" },
     { label: "DESC", value: "desc" },
-    { label: "Release Date", value: "release_date" },
-    { label: "Latest Post", value: "latest_post" },
+    { label: "Release Date ASC", value: "release_date_asc" },
+    { label: "Release Date DESC", value: "release_date_desc" },
   ];
 
   return (
