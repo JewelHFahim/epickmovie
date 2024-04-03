a66b95bee57b0a18272b06f80248bd6d7c8e6688
JewelHFahim <jewelhfahim@gmail.com>
2024-04-04 01:37:44 +0600

    updated

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -2,4 +2,5 @@ export const base_url = "https://testepick.emdb.online/api";
 export const admin_base_url = "http://localhost:3001";
 export const user_api_key = "dtmgNfrv6AJDXV3nPEhkaQ";
 
-export const theme = "theme";
+// theme  switch
+export const theme = "theme"; // "theme1" 
